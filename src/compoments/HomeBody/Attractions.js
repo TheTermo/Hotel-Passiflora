998829b145c26ccb083158a7d06d91fdcade67a9
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 <it-jg@o2.pl>
2024-01-25 13:49:21 +0100

    passiflora

--- src/compoments/HomeBody/Attractions.js
+++ src/compoments/HomeBody/Attractions.js
@@ -1,6 +1,8 @@
 import "./attractions.scss"
 import * as React from "react";
-import Atrakcja1 from "../../images/wang.JPG"
+import Atrakcja1 from "../../images/snieg.jpg"
+import Atrakcja2 from "../../images/wang.JPG"
+import Atrakcja3 from "../../images/Kolorowa.jpg"
 
 function Attractions(){
     return(
@@ -13,13 +15,12 @@ function Attractions(){
                     <div className="t-image">
                         <img alt="HeaderImage" src={Atrakcja1}/>
                     </div>
-                    <h4>Wang</h4>
-                    <p>Ewangelicki kościół parafialny w Karpaczu w Karkonoszach, przeniesiony w 1842 z miejscowości Vang,
-                        leżącej nad jeziorem Vangsmjøsa w Norwegii.</p>
+                    <h4>Śnieżka</h4>
+                    <p>Śnieżka znacznie góruje nad otaczającymi ją grzbietami. Wznosi się w zachodniej części Czarnego Grzbietu. Na zachodzie Przełęcz pod Śnieżką oddziela ją od Równi pod Śnieżką</p>
                 </div>
                 <div className="t-card">
                     <div className="t-image">
-                        <img alt="HeaderImage" src={Atrakcja1}/>
+                        <img alt="HeaderImage" src={Atrakcja2}/>
                     </div>
                     <h4>Wang</h4>
                     <p>Ewangelicki kościół parafialny w Karpaczu w Karkonoszach, przeniesiony w 1842 z miejscowości Vang,
@@ -27,11 +28,11 @@ function Attractions(){
                 </div>
                 <div className="t-card">
                     <div className="t-image">
-                        <img alt="HeaderImage" src={Atrakcja1}/>
+                        <img alt="HeaderImage" src={Atrakcja3}/>
                     </div>
-                    <h4>Wang</h4>
-                    <p>Ewangelicki kościół parafialny w Karpaczu w Karkonoszach, przeniesiony w 1842 z miejscowości Vang,
-                        leżącej nad jeziorem Vangsmjøsa w Norwegii.</p>
+                    <h4>Tor saneczkowy</h4>
+                    <p>Wózkiem może zjeżdżać jedna lub dwie osoby. Czas przejazdu trwa od 5 do 7 minut.
+                        Tor nie posiada ograniczeń wiekowych, jednak dzieci do lat 8 muszą zjeżdżać z osobą pełnoletnią.</p>
                 </div>
             </div>
         </div>
